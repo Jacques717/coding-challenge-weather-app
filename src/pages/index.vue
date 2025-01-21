0ababac12aeb73c1d0688a11301992a0d65992ca
Spencer Steffen <spencer@oatsovernight.com>
2025-01-21 15:51:21 -0800

    initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex items-center justify-center p-10">
+    <div>
+      <h1 class="text-xl">
+        Weather App
+      </h1>
+      <p>Please see README.md to get started.</p>
+    </div>
+  </div>
+</template>
