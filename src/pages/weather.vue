27d4aae511ac7777b08f22a8ece9b7228f9f323c
Jacques de Kock <jacques717@hotmail.com>
2025-01-26 19:15:28 -0500

    disable refresh button while weather is loading

--- src/pages/weather.vue
+++ src/pages/weather.vue
@@ -21,12 +21,26 @@
       </div>
       <button 
         @click="fetchWeatherData(true)"
-        class="bg-white/20 hover:bg-white/30 rounded-full p-4 transition-colors inline-flex items-center gap-2"
+        class="bg-white/20 hover:bg-white/30 disabled:opacity-50 disabled:cursor-not-allowed rounded-full p-4 transition-colors inline-flex items-center gap-2"
+        :disabled="loading || loadingRandom"
       >
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <img 
+          v-if="loadingRandom"
+          src="https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/wind.svg" 
+          class="w-6 h-6"
+          alt="Loading..."
+        />
+        <svg 
+          v-else
+          xmlns="http://www.w3.org/2000/svg" 
+          class="h-6 w-6" 
+          fill="none" 
+          viewBox="0 0 24 24" 
+          stroke="currentColor"
+        >
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
         </svg>
-        Try Another Location
+        {{ loadingRandom ? 'Loading...' : 'Try Another Location' }}
       </button>
     </div>
 
